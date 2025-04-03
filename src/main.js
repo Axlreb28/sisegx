a18a3f6cf1480714b2d2c8320c6b15496b2c4272
Rockolott <pataponivan@gmail.com>
2025-04-03 12:58:33 -0500

    iconos

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import './assets/css/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
+// Importar las dependencias de FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserShield, faDatabase, faPalette } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Agregar los íconos a la biblioteca global
+library.add(faUserShield, faDatabase, faPalette)
+
 const app = createApp(App)
+
+// Registrar FontAwesomeIcon como un componente global
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
